ff3b0090b69aaf7b4603afafa5ab2696865b412c
ju8ko <julia.x.kobzar@gmail.com>
2022-06-29 14:00:32 +0200

    Exercise is done

--- solution.js
+++ solution.js
@@ -1,1 +1,24 @@
 // write your code in this file
+
+//1. Console log this line of text: "This is an exercise in console logging" (do not forget quotation marks).
+
+console.log('"This is an exercise in console logging"');
+
+//2.Declare a variable called exercise and give it the following value: "This is the value.". Console log the exercise variable.
+const exercise = "This is the value.";
+console.log(exercise);
+
+//3.
+const firstName = "Julia";
+const lastName = "Kobzar";
+console.log(firstName, lastName);
+
+//4.
+const city = "Berlin";
+const job = "Student in DCI";
+const age = 30;
+console.log(firstName, lastName, age, city, job);
+console.log(
+  firstName,
+  lastName + " is a " + age + " year old " + job + " who lives in " + city
+);
